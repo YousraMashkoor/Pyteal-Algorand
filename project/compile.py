19fce9bb3989e23921c62540c5f3a51001a251d7
YousraMashkoor <yousra.mashkoor@gmail.com>
2022-06-21 10:17:13 +0500

    first contract creation

--- project/compile.py
+++ project/compile.py
@@ -7,22 +7,22 @@ if __name__ == "__main__":
     mod = sys.argv[1]
 
     try:
-        approval_out = sys.argv[2]
+        approval_out = sys.argv[2] # reads the two output file approval and clear
     except IndexError:
         approval_out = None
 
     try:
-        clear_out = sys.argv[3]
+        clear_out = sys.argv[3] # reads the two output file approval and clear
     except IndexError:
         clear_out = None
 
-    contract = importlib.import_module(mod)
+    contract = importlib.import_module(mod) # reads into module
 
     if approval_out is None:
-        print(program.application(contract.approval()))
+        print(program.application(contract.approval())) # calls approval fuction
     else:
         with open(approval_out, "w") as h:
-            h.write(program.application(contract.approval()))
+            h.write(program.application(contract.approval())) # calls clear fuction
 
     if clear_out is not None:
         with open(clear_out, "w") as h:
